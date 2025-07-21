28974fe0a2eb22c60c9ed31e26772e8c06e2899b
victorocna <victorocnarescu@gmail.com>
2025-07-21 11:29:59 +0300

    Fix is move active logic

--- functions/is-move-active.js
+++ functions/is-move-active.js
@@ -1,9 +1,16 @@
-const isMoveNumber = (current, moment) => {
+/**
+ * Checks if the current move is active by comparing it with a given moment.
+ */
+const isMoveActive = (current, moment) => {
   try {
-    return current.fen === moment.fen && current.move === moment.move;
+    return (
+      current.fen === moment.fen && // Compare FEN strings
+      current.move === moment.move && // Compare moves
+      current.depth === moment.depth // Compare depth
+    );
   } catch (err) {
     return false;
   }
 };
 
-export default isMoveNumber;
+export default isMoveActive;
