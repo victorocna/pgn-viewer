8f85156cf785b9af52ebf2dead468ccc66ac0e93
victorocna <victorocnarescu@gmail.com>
2025-07-23 17:11:46 +0300

    Add fonts to bundle

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import url from '@rollup/plugin-url';
+import copy from 'rollup-plugin-copy';
 import external from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import pkg from './package.json';
@@ -25,11 +26,16 @@ const config = {
       extract: 'bundle.css',
       minimize: true,
     }),
+    copy({
+      targets: [{ src: 'assets/fonts/*', dest: 'dist/fonts' }],
+    }),
     resolve(),
     commonjs({ extensions: ['.js', '.jsx'] }),
     url({
       fileName: '[name][extname]',
       limit: 100000,
+      include: ['**/*.woff', '**/*.woff2', '**/*.eot', '**/*.ttf', '**/*.otf'],
+      emitFiles: true,
     }),
   ],
 };
