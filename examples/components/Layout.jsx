7dd46ecaa43c10e3b95016cd0623dd5bf306ec82
victorocna <victorocnarescu@gmail.com>
2025-07-23 17:13:19 +0300

    Update styles

--- examples/components/Layout.jsx
+++ examples/components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ title, children }) => {
       <div className="font-body text-sm min-h-screen bg-gray-100 flex">
         <Menu />
         <main className="max w-full lg:col-span-5 p-4 lg:p-8 xl:px-12 gap-4">
-          <div className="flex items-center mb-12">
+          <div className="flex items-center mb-4">
             <div className="flex flex-1">
               <h3 className="text-2xl font-semibold">{title}</h3>
             </div>
